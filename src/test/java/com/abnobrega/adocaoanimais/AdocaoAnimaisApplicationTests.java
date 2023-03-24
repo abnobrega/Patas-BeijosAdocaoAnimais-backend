d021d8e51ee328b1946a8f8d52413908c888155f
abnobrega <abonturi@gmail.com>
2023-03-24 20:37:46 -0300

    Projeto Iniciado

---
+++ src/test/java/com/abnobrega/adocaoanimais/AdocaoAnimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnobrega.adocaoanimais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoAnimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
